7b3911551f66fc0b13267bf295431d6f022be1b7
occamskatana <johngallweycarter@gmail.com>
2016-04-11 23:36:38 -0400

    4 complete

--- app/scripts/controllers/LandingController.js
+++ app/scripts/controllers/LandingController.js
@@ -3,8 +3,8 @@
 
 		$scope.active = false
 		$scope.breakActive = false;
-
 		$scope.timerTime = 1500;
+		$scope.completedSessions = 0
 
 		
 
@@ -40,11 +40,32 @@
 
 		$scope.updateTimer = function(){
 			onBreak = false;
+
+			if ($scope.completedSessions % 4 === 0 && $scope.completedSessions !== 0) {
+				$scope.timerTime = (60 * 30);
+			} else {
+				$scope.timerTime = 1500;
+			};
+
 			if ($scope.active === false){
 				promise = $interval(function(){
 					$scope.timerTime = $scope.timerTime - 1;
+					if($scope.timerTime === 0){
+						$interval.cancel(promise);
+						$scope.completedSessions += 1;
+						if ($scope.completedSessions % 4 === 0 && $scope.completedSessions !== 0) {
+							$scope.timerTime = (60 * 30);
+						}
+
+						if ($scope.completedSessions % 5 === 0 ) {
+							$scope.updateBreakTimer();
+						}
+
+						$scope.active = false;
+					}
 				}, 1000)
 				$scope.active = true;
+				
 			} else {
 				$interval.cancel(promise);
 				$scope.timerTime = 1500;
